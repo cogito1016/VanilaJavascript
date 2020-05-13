a2958a58872c28eefc6c340e8276a6ce6471be14
cogito1016 <cogito1016@naver.com>
2020-05-14 03:29:40 +0900

    EventListener in vanilaJS and event Object

--- test/test.js
+++ test/test.js
@@ -8,7 +8,6 @@
 // `Hello ${name} you are ${age} years old` 이렇게 쓸 수 있다.
 //MVC패턴에서 View로 보내준 Model을 사용할때를 보는것같다.
 
-
 //자바스크립트는 HTML을 객체로 가져올 수 있다.
 //Okay Jquery에 있던 $("#")이다. 명령어는 더 길지만 다 가볍고 빠르다.
 const title = document.getElementById("title");
@@ -26,3 +25,19 @@ console.dir(title);
 // . 키워드를통해 객체가가지는 속성에 접근
 title.style.color='red';
 document.title="안녕 ";
+
+//자바스크립트는 위에처럼 HTML과 CSS를 다루기 위해서만 만들어진것이 아님
+//이벤트에 대응하기 위해 만들어짐 click resize submit . . .
+
+function handleResize(event){//event는 이벤트리스너의 콜백함수에 자동적으로 붙여지는 이벤트객체 이다
+    console.log("I have been resized"+ event);
+}//handleResize end
+
+//윈도우에 리사이즈 이벤트가발생하면 handleResize함수가 실행된다.
+window.addEventListener("resize",handleResize);
+
+function handleClick(){
+    title.style.color='white';
+}//handleClick end
+
+title.addEventListener("click",handleClick);
