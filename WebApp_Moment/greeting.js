286c1957b106cb100da671bf1c4829ad32dcc3c3
cogito1016 <cogito1016@naver.com>
2020-05-14 10:44:39 +0900

    localStorage vs. essionStorage & setItem getItem

--- WebApp_Moment/greeting.js
+++ WebApp_Moment/greeting.js
@@ -8,6 +8,28 @@ const greeting = document.querySelector(".js-greetings");
 const USER_LS = 'currentUser';
 const SHOING_CN ="showing";
 
+//로컬스트로지 vs 세션스토리지
+//로컬스토리지는 유효기간이 영구적이며, 세션스토리지는 브라우저가 열려있는 기간동안만 유효하다.
+//로컬스토리지는 key-val값쌍으로 이루어져있다.
+//크롬개발자도구에서는 sotrage-localstorage에서 확인 가능하다
+function saveName(text){
+   localStorage.setItem(USER_LS,text); //setItem을 통해 로컬스토리지에 저장할 수 있음 
+   //(객체의경우 Json.stringify()로 스트링으로 변환 후 보내주어야함)
+   //만약 문자열로변환된 객체를 받고나서 다시 객체로 변환하고싶다면, Json.parse()로 변환가능
+}//saveName() end
+
+function handleSubmit(event){
+    event.preventDefault(); //기본적으로 동작하는 제출동작을 막는다(url로 전송하여 리프레쉬하는)
+    const currentValue = input.value; //InnerText는 파라미터를 포함하는 설정자, value는 파라미터가없는 접근자
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}//handleSubmit() end
+
+function askForName(){
+    form.classList.add(SHOING_CN);
+    form.addEventListener("submit",handleSubmit);
+}//askForName() end
+
 function paintGreeting(text){
     //해당 요소객체에 .classList를 통해 클래스리스트에 접근할 수 있다.
     //Jquery에서의 .addClass .removeClass와 동일
@@ -20,8 +42,9 @@ function paintGreeting(text){
 
 function loadName(){
     const currentUser = localStorage.getItem(USER_LS);
-    if(currentUser === null){
+    if(currentUser === null){// === 연산자는 강력한 비교연산자이다 같은'지료형'인가 같은'값'인가 까지 비교한다 
         //she is not
+        askForName();
     }else{
         //she is
         paintGreeting(currentUser);
