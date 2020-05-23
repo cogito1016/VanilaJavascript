ac3228a81422c14d2cae53a26ae437841e7fbc8a
cogito1016 <cogito1016@naver.com>
2020-05-23 10:57:09 +0900

    TestSoultion make right function 'pow' and create testcase

--- TestSolution/test.js
+++ TestSolution/test.js
@@ -1,19 +1,15 @@
 describe("pow", function() {
 
-    //방법1
-    it("주어진 숫자의 n 제곱", function() {
-      assert.equal(pow(2, 3), 8);
-      assert.equal(pow(3,4), 81);
-    });
+    function makeTest(x) {
+        let result = x*x*x;
+        it(`${x}의 3제곱=${result}`, function() {
+            assert.equal(pow(x, 3), result);
+        }); 
+    }//makeTest() end
 
-    //방법2
-    it("2의 3 제곱", function() {
-        assert.equal(pow(2, 3), 8);
-      }); 
+    for(let i=1;i<=5;i++){
+        makeTest(i);    
+    }//for end
 
-    //방법2
-    it("3의 4 제곱", function() {
-    assert.equal(pow(3,4), 81);
-    }); 
   
   });
