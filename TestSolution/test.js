3fc27b752c388b0e66d72f0ccfc4002c93f292bc
cogito1016 <cogito1016@naver.com>
2020-05-23 11:27:36 +0900

    TestSoultion make other describe section and add testCase( Case. result NaN )

--- TestSolution/test.js
+++ TestSolution/test.js
@@ -1,15 +1,23 @@
 describe("pow", function() {
 
-    function makeTest(x) {
-        let result = x*x*x;
-        it(`${x}의 3제곱=${result}`, function() {
-            assert.equal(pow(x, 3), result);
-        }); 
-    }//makeTest() end
+    describe("정수를 3제곱합니다",function() {
 
-    for(let i=1;i<=5;i++){
-        makeTest(i);    
-    }//for end
+        function makeTest(x) {
+            let result = x*x*x;
+            it(`${x}의 3제곱=${result}`, function() {
+                assert.equal(pow(x, 3), result);
+            }); 
+        }//makeTest() end
+        for(let i=1;i<=5;i++){
+            makeTest(i);    
+        }//for end
+    });
 
+    describe("'음수'번을 제곱려고하면 IsNan을 반환합니다",function() {
+
+        it("2를 -1번 제곱하려고 할때",function() {
+            assert.isNaN(pow(2,-1),NaN);
+        });
+    });
   
   });
