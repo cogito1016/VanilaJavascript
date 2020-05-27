28bafd12822e35d69fbc47de5effd71c6a2b8378
cogito1016 <cogito1016@naver.com>
2020-05-27 16:06:18 +0900

    LexicalEnvironment [[environment]] property

--- LexicalEnvironment/example2.js
+++ LexicalEnvironment/example2.js
@@ -1,1 +1,15 @@
-
+function makeWorker() {
+    let name = "Pete";
+  
+    return function() {
+      alert(name);
+    };
+  }//makeWorker() end
+  
+  let name = "John";
+  
+  // create a function
+  let work = makeWorker();
+  
+  // call it
+  work(); // what will it show?
