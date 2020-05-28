5965475ccb02e73e1b4e5291de763005010eb1b6
cogito1016 <cogito1016@naver.com>
2020-05-28 19:15:21 +0900

    new Keyword - Constructor Function & Protocol 1- First word is upper word , Protocol-2 For using constructor Function, Use 'new' keyword

--- Basic/newAndThis.js
+++ Basic/newAndThis.js
@@ -16,4 +16,96 @@ let user = {
     }//sayHi() end
 }//user end
 
-user.sayHi();
+// user.sayHi();
+
+
+// function makeUser() {
+//     return {
+//         name: "John",
+//         ref: this//여기에 바로 this를 써주는것은 makeUser의 this를 참조하는것이다.
+//     };
+// }//makeUser() end
+  
+// let user = makeUser();
+// alert( user.ref.name ); // 결과가 어떻게 될까요?
+
+
+
+// let calculator = {
+//     // ... 여기에 답안 작성 ...
+//     a:0,
+//     b:0,
+//     read(){
+//         let tempA, tempB;
+//         this.a=parseInt(prompt("숫자를 입력해주세요 두개",0));
+//         this.b=parseInt(prompt("숫자를 입력해주세요 두개",0));
+//     },
+//     sum(){
+//         return(this.a+this.b);
+//     },
+//     mul(){
+//         return(this.a*this.b);
+//     }
+//   };
+//   calculator.read();
+//   alert( calculator.sum() );
+//   alert( calculator.mul() );
+
+// let ledder = {
+//     step:0,
+//     up(){
+//         this.step++;
+//         return this;
+//     },
+//     down(){
+//         this.step--;
+//         return this;
+//     },
+//     showStep(){
+//         alert(this.step);
+//     }
+// }
+
+// ledder.up().up().down().showStep();
+
+// let obj = new Object(); 
+// function A() { 
+//     return obj;
+//  }
+// function B() {
+//     return obj;
+//  }
+// let a = new A;
+// let b = new B;
+// alert( a == b ); // true
+
+// function Calculator(){
+
+//     this.read =function(){ 
+//         this.a=parseInt(prompt("숫자입력",0)); 
+//         this.b=parseInt(prompt("숫자입력",0)); 
+//     };
+//     this.sum = function(){ return (this.a+this.b);}
+//     this.mul = function(){ return (this.a*this.b);}
+    
+// }//Calculator() end
+
+// let calculator = new Calculator();
+// calculator.read();
+
+// alert( "Sum=" + calculator.sum() );
+// alert( "Mul=" + calculator.mul() );
+
+function Accumulator(int){
+    this.value=int;
+    this.read = function(){ this.value+=parseInt(prompt("값",0));}
+}//Acccumulator( )end
+
+let accumulator = new Accumulator(1); // 최초값: 1
+
+accumulator.read(); // 사용자가 입력한 값을 더해줌
+accumulator.read(); // 사용자가 입력한 값을 더해줌
+
+alert(accumulator.value); // 최초값과 사용자가 입력한 모든 값을 더해 출력함
+
+
