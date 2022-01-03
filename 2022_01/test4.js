0a5a333f2fd703c36c17be31d1efa0ebd4853aa1
cogito1016 <cogito1016@naver.com>
2022-01-03 10:40:06 +0900

    refactor: 기본값객체 - Object.assign

--- 2022_01/test4.js
+++ 2022_01/test4.js
@@ -11,13 +11,15 @@ const book = {
 const defaultKeys = Object.keys(defaults);
 const bookKeys = Object.keys(book);
 
-for (let i = 0; i < defaultKeys.length; i++) {
-  let defaultKey = defaultKeys[i];
-  let defaultValue = defaults[defaultKey];
-  //   if (book[defaultKey] === undefined) {
-  //     book[defaultKey] = defaultValue;
-  //   }
-  book[defaultKey] = book[defaultKey] || defaultValue;
-}
+// for (let i = 0; i < defaultKeys.length; i++) {
+//   let defaultKey = defaultKeys[i];
+//   let defaultValue = defaults[defaultKey];
+//   //   if (book[defaultKey] === undefined) {
+//   //     book[defaultKey] = defaultValue;
+//   //   }
+//   book[defaultKey] = book[defaultKey] || defaultValue;
+// }
+
+Object.assign(book, defaults);
 
 console.log(book);
